1c391992f3cc6aeef61f3127ccaf4b28db993052
xistri <cristian93marti@gmail.com>
2022-11-08 16:51:25 +0100

    modify gitignore

---
+++ js/index.test.js
@@ -0,0 +1,11 @@
+// Lo que había hecho
+// import { arrayLength } from './index.js';
+
+// const arrayL = ['a', 'b', 'c'];
+
+// describe('Given arrayLength function', () => {
+//     test(`The array lenght of ${arrayL} should be ${arrayL.length}`, (array) => {
+//         const result = arrayLength(arrayL);
+//         expect(result).toBe(arrayL.length);
+//     });
+// });
